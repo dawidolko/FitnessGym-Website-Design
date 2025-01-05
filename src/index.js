8ed97c419f4481119f9d3126042dd77d3ef5a11a
dawidolko <dawid_olko@outlook.com>
2025-01-05 03:22:19 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/FitnessGym">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
